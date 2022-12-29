3bc6a0c728d2cdff3451c55cc1ae285a236c32c9
Vlada <80097541+Rafaelkaaa@users.noreply.github.com>
2022-12-29 13:39:35 +0200

    Formatting class SumCalculator

--- src/main/java/SumCalculator.java
+++ src/main/java/SumCalculator.java
@@ -1,10 +1,10 @@
 public class SumCalculator {
 
-    public int sum(int userDigit) throws IllegalArgumentException{
+    public int sum(int userDigit) throws IllegalArgumentException {
         if (userDigit == 0) throw new IllegalArgumentException("Enter number bigger than 0");
         int result = 0;
         for (int i = 0; i <= userDigit; i++) {
-            result+=i;
+            result += i;
         }
         return result;
     }
